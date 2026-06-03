ed1c40fc6925cbc606fb795fa97ca8e906e2f9f0
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2026-06-03 13:18:51 +0600

    add small-caps to lang card

--- src/cards/top-languages.js
+++ src/cards/top-languages.js
@@ -911,6 +911,7 @@ const renderTopLanguages = (topLangs, options = {}) => {
     }
     .stat {
       font: 600 14px 'Segoe UI', Ubuntu, "Helvetica Neue", Sans-Serif; fill: ${colors.textColor};
+      font-variant: small-caps;
     }
     @supports(-moz-appearance: auto) {
       /* Selector detects Firefox */
@@ -920,6 +921,7 @@ const renderTopLanguages = (topLangs, options = {}) => {
     .lang-name {
       font: 400 11px "Segoe UI", Ubuntu, Sans-Serif;
       fill: ${colors.textColor};
+      font-variant: small-caps;
     }
     .stagger {
       opacity: 0;
